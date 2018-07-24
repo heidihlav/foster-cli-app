f98116fa1160e4595fa464fc8ff80c47d92b2ec6
Heidi Hlavinka <heidi@Heidis-MacBook-Pro.local>
2018-07-23 21:18:37 -0500

    Added code to Pets class and tested CLI - getting NameError.

--- lib/environment.rb
+++ lib/environment.rb
@@ -8,7 +8,7 @@ end
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
-require_relative "./foster/version"
 require_relative "./foster/cli"
-require_relative "./foster/scraper"
 require_relative "./foster/pets"
+require_relative "./foster/scraper"
+require_relative "./foster/version"
--- lib/foster/pets.rb
+++ lib/foster/pets.rb
@@ -1,11 +1,18 @@
 # code pets objects here - individual pets attributes
 class Foster::Pets
-
-
   attr_accessor :name, :description, :url, :cats, :dogs
+  @@all = []
 
+  def initialize(name, description, url)
+    @name = name
+    @description = description
+    @url = url
+    @@all << self
+  end
 
-  #@@all
+  def self.all
+    @@all
+  end
 
   #each or each with index - diff iterators
 
