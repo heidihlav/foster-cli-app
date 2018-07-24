647885147f7d42ba9f1f3eaeb7165b4846246921
Heidi Hlavinka <heidi@Heidis-MacBook-Pro.local>
2018-07-23 20:13:54 -0500

    Added in line for spacing between prompts in all class methods.

--- lib/foster/cli.rb
+++ lib/foster/cli.rb
@@ -9,6 +9,7 @@ class CLI
 
   def self.main_menu
     puts "Do you want to learn about one of our cats or dogs available for foster? Enter 'cats' or 'dogs'."
+    puts ""
     puts "If you'd like to exit at any time, type 'exit'."
     user_input = gets.strip
     if user_input == "cats"
@@ -16,13 +17,14 @@ class CLI
     elsif user_input == "dogs"
        dogs
     elsif user_input == 'exit'
-         puts "Thank you for considering one of our foster pets!"
+       puts "Thank you for considering one of our foster pets!"
      end
   end
 
   def self.display_dogs   #displays dogs in a list
     # @dogs in initialize and scrape dogs section
     puts "Here are our urgent needs for Dogs:"
+    puts ""
     @dogs.each.with_index(1) do |dog, index|
       puts "#{index}. #{dog.name}"
     end
@@ -31,14 +33,15 @@ class CLI
   def self.display_cats   #displays cats in a list
     # @cats in initialize and scrape cats section
     puts "Here are our urgent needs for Cats:"
+    puts ""
     @cats.each.with_index(1) do |cat, index|
       puts "#{index}. #{cat.name}"
     end
   end
 
   def self.choose_cat_or_dog
-    puts ""
     puts "Which cat or dog would you like to know more about? Enter a number."
+    puts ""
     user_input = gets.strip
     index = input.to_i - 1
     #needs to go through scraped indiv pet pages similar to cernan's method
@@ -50,9 +53,10 @@ class CLI
 
   def self.another_cat_or_dog
     puts "Would you like to see another cat or dog? Enter Y or N."
+    puts ""
     user_input = gets.strip
     if user_input == "Y"
-      choose_cat_or_dog #go back to "Which cat or dog..."*
+      self.choose_cat_or_dog #go back to "Which cat or dog..."*
     elsif user_input == "N"
       puts "Thank you for considering one of our foster pets! If you are ready to apply, please contact us!"
       #exits the program
