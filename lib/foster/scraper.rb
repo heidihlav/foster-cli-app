84b7eb7e87ed23a421a203a06d3e87038d87b06f
Heidi Hlavinka <heidi@Heidis-MacBook-Pro.local>
2018-07-26 21:17:55 -0500

    Removed notes from scraper class to make testing easier.

--- lib/foster/scraper.rb
+++ lib/foster/scraper.rb
@@ -1,27 +1,15 @@
-
-# Here is for the name... doc.css("div.callout a").each{|li| puts "#{li.text}"}
-# Here is for the url... doc.css("div.callout").each{|li| puts "#{li.css('a')[0]['href']}"}
-
 class Foster::Scraper
+
   def scrape_names
-      # puts "SCRAPE names"
-      doc = Nokogiri::HTML(open('http://www.friends4life.org/how-to-help/foster'))
-        @name = doc.css("div.callout li a") #use li because foster care app is an a item
-         @name.each {|li| puts "#{li.text}"}
-      # @detail.each do |li|
-      #   if li.css("a").any?  # to eliminate headlines without detailed story
-      #     headline = BizNews::Headline.new
-      #     headline.title = li.text.strip
-      #     headline.url = "https://www.cnbc.com#{li.css("a")[0]["href"]}"
-      #     headline.save
-      #   end
-      # end
-      # BizNews::Headline.all
+    doc = Nokogiri::HTML(open('http://www.friends4life.org/how-to-help/foster'))
+      @name = doc.css("div.callout li a") #use li because foster care app is an a item
+       @name.each {|li| puts "#{li.text}"}
   end
 
   def scrape_url
     doc = Nokogiri::HTML(open('http://www.friends4life.org/how-to-help/foster'))
       @url = doc.css("div.callout").each {|li| puts "#{li.css('a')[0]['href']}"}
+      binding.pry
   end
 
   def scrape_subheads
@@ -29,14 +17,5 @@ class Foster::Scraper
       @subheading = doc.css("div.callout h5").first.text  #Urgent needs for dogs heading
       @subheading = doc.css("div.callout h5")[1].text  #Urgent needs for cats heading
   end
-  # def scrape_url(headline, url)
-  #   # puts "SCRAPING #{url}"
-  #   article_doc = Nokogiri::HTML(open("#{url}"))
-  #   article = BizNews::Article.new
-  #   article.title = headline
-  #   article.author = article_doc.search("div.source a[itemprop='url']").text
-  #   article.content = article_doc.search("div#article_body p").text.strip
-  #   article.save
-  #   article
-  # end
+
 end
