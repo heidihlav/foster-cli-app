0e0dab0a843d43034290a707e9c856504d44a662
Heidi Hlavinka <heidi@Heidis-MacBook-Pro.local>
2018-08-04 16:56:53 -0500

    Restructuring executable and CLI

--- lib/foster/cli.rb
+++ lib/foster/cli.rb
@@ -2,58 +2,56 @@
 class Foster::CLI
 
   def start
-    puts "Welcome to Friends for Life Foster page!"
-    puts ""
-    main_menu
+    puts "Cats or dogs"
   end
 
-  def main_menu
-    puts "If you want to learn about one of our pets available for foster, enter Y."
-    puts ""
-    puts "If you'd like to exit at any time, type 'exit'."
-    user_input = gets.strip
-    if user_input == "Y"
-       display_pets
-    elsif user_input == 'exit'
-       puts "Thank you for considering one of our foster pets!"
-     end
-  end
-
-  def display_pets   #displays all pets in a list
-    puts "Here are all of our pets available for foster:"
-    puts ""
-    Foster::Scraper.scrape_names
-    pets = Foster::Pets.all #each_with_index for all pets - choose # instead of name
-    pets.each.with_index(1) do |pet, index|
-      puts "#{index}. #{pet.name}"
-    end
-    choose_pet
-  end
-
-  def choose_pet
-    puts "Which pet would you like to know more about? Enter the number next to their name."
-    puts ""
-    user_input = gets.strip
-    index = user_input.to_i - 1
-    Foster::Scraper.get_pet_description(url)
-    pets = Foster::Pets.all[index]
-    puts "Here's information about our pet:"
-    # puts Foster::Pets.list_pets - need to make method to list pet descriptions from array
-    Foster::CLI.new.another_pet
-  end
-
-  def another_pet
-    puts "Would you like to see another pet? Enter Y or N."
-    puts ""
-    user_input = gets.strip
-    if user_input == "Y"
-      self.choose_pet #go back to "Which cat or dog..."*
-    elsif user_input == "N"
-      puts "Thank you for considering one of our foster pets! If you are ready to apply, please contact us!"
-    end
-  end
-
- 
-
-
+#   def main_menu
+#     puts "If you want to learn about one of our pets available for foster, enter Y."
+#     puts ""
+#     puts "If you'd like to exit at any time, type 'exit'."
+#     user_input = gets.strip
+#     if user_input == "Y"
+#        display_pets
+#     elsif user_input == 'exit'
+#        puts "Thank you for considering one of our foster pets!"
+#      end
+#   end
+#
+#   def display_pets   #displays all pets in a list
+#     puts "Here are all of our pets available for foster:"
+#     puts ""
+#     Foster::Scraper.scrape_names
+#     pets = Foster::Pets.all #each_with_index for all pets - choose # instead of name
+#     pets.each.with_index(1) do |pet, index|
+#       puts "#{index}. #{pet.name}"
+#     end
+#     choose_pet
+#   end
+#
+#   def choose_pet
+#     puts "Which pet would you like to know more about? Enter the number next to their name."
+#     puts ""
+#     user_input = gets.strip
+#     index = user_input.to_i - 1
+#     Foster::Scraper.get_pet_description(url)
+#     pets = Foster::Pets.all[index]
+#     puts "Here's information about our pet:"
+#     # puts Foster::Pets.list_pets - need to make method to list pet descriptions from array
+#     Foster::CLI.new.another_pet
+#   end
+#
+#   def another_pet
+#     puts "Would you like to see another pet? Enter Y or N."
+#     puts ""
+#     user_input = gets.strip
+#     if user_input == "Y"
+#       self.choose_pet #go back to "Which cat or dog..."*
+#     elsif user_input == "N"
+#       puts "Thank you for considering one of our foster pets! If you are ready to apply, please contact us!"
+#     end
+#   end
+#
+#
+#
+#
 end
