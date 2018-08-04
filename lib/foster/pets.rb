bde114a1e4147e9cfd919fc3ff531e680b876b9d
Heidi Hlavinka <heidi@Heidis-MacBook-Pro.local>
2018-08-04 18:25:03 -0500

    Begin testing CLI and object class interaction.

--- lib/foster/pets.rb
+++ lib/foster/pets.rb
@@ -1,18 +1,27 @@
 # code pets objects here - individual pets attributes
 class Foster::Pets
 
-  attr_accessor :name, :description, :url
+  # attr_accessor :name, :description, :url
   @@all = []
 
-  def initialize(name, description, url)
-    @name = name
-    @url = url
-    @description = description
-    @@all << self
-  end
+  # def initialize(name, description, url)
+  #   @name = name
+  #   @url = url
+  #   @description = description
+  #   @@all << self
+  # end
 
   def self.all
-    @@all
+    puts "These are our cats or dogs available for foster:"
+    puts ""
+    puts "Dogs"
+    puts "1. Fido"
+    puts "2. Fluffy"
+    puts "Cats"
+    puts "3. Mittens"
+    puts "4. Socks"
+    puts ""
+    # @@all  #return instances of pets
   end
 
   # def self.save
