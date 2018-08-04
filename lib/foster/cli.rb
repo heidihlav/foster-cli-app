1ccb9c607be90d409b76bd107644fd1ee96c58e8
Heidi Hlavinka <heidi@Heidis-MacBook-Pro.local>
2018-08-04 17:47:14 -0500

    Completed preliminary CLI class.

--- lib/foster/cli.rb
+++ lib/foster/cli.rb
@@ -4,30 +4,45 @@ class Foster::CLI
   def start
     list_pets
     menu
+    goodbye
   end
 
   def list_pets
     puts "These are our cats or dogs available for foster:"
+    puts ""
     puts "Dogs"
     puts "1. Fido"
     puts "2. Fluffy"
     puts "Cats"
     puts "3. Mittens"
     puts "4. Socks"
+    puts ""
   end
 
   def menu
-    puts "Which cat or dog would you like to know more about?"
-    user_input = gets.strip
-    case user_input
-    when "1"
-      puts "Follow this link to learn more about our pet: Fido link"
-    when "2"
-      puts "Follow this link to learn more about our pet: Fluffy link"
-    when "3"
-      puts "Follow this link to learn more about our pet: Mittens link"
-    when "4"
-      puts "Follow this link to learn more about our pet: Socks link"
+    user_input = nil
+    while user_input != "exit"
+      puts "Which cat or dog would you like to know more about? Type the number next to the pet's name. Type 'exit' if you are done. Or type 'list' to see the available pets again."
+      user_input = gets.strip
+      case user_input
+      when "1"
+        puts "Follow this link to learn more about our pet: Fido link"
+      when "2"
+        puts "Follow this link to learn more about our pet: Fluffy link"
+      when "3"
+        puts "Follow this link to learn more about our pet: Mittens link"
+      when "4"
+        puts "Follow this link to learn more about our pet: Socks link"
+      when "list"
+        list_pets
+      else
+        "Enter 'list' or 'exit'."  
+      end
+    end
+  end
+
+  def goodbye
+    "Thank you for considering one of our pets to foster!"
   end
 
 end
