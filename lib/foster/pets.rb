875153712289877b613c0f2fd1f6a77d8a91475b
Heidi Hlavinka <heidi@Heidis-MacBook-Pro.local>
2018-08-04 20:08:53 -0500

    Start on scraper methods - will need to move to scraper class.

--- lib/foster/pets.rb
+++ lib/foster/pets.rb
@@ -11,18 +11,34 @@ class Foster::Pets
   # end
 
   def self.all
+    self.scrape_pets
   #scrape ffl and return pets
-    pet_1 = Foster::Pets.new
-    pet_1.name = "Marfa"
-    pet_1.url = "http://www.friends4life.org/adopt/adoptable-dogs/adoptable/5227/7631/Marfa"
-
-    pet_2 = Foster::Pets.new
-    pet_2.name = "Jane"
-    pet_2.url = "http://www.friends4life.org/adopt/adoptable-dogs/adoptable/2266/3504/Jane"
-
-    [pet_1, pet_2]# @@all  #return instances of pets in array
-
+  # Foster::Scraper.new.scrape_names
+    # pet_1 = Foster::Pets.new
+    # pet_1.name = "Marfa"
+    # pet_1.url = "http://www.friends4life.org/adopt/adoptable-dogs/adoptable/5227/7631/Marfa"
+    #
+    # pet_2 = Foster::Pets.new
+    # pet_2.name = "Jane"
+    # pet_2.url = "http://www.friends4life.org/adopt/adoptable-dogs/adoptable/2266/3504/Jane"
+    #
+    # [pet_1, pet_2]# @@all  #return instances of pets in array
   end
 
+  def self.scrape_pets
+    pets = []
+
+    pets << self.scrape_names
+    pets# => [pet_1, pet_2]
+  end
+
+  def self.scrape_names
+    doc = Nokogiri::HTML(open('http://www.friends4life.org/how-to-help/foster'))
+    pet_names = doc.css(".callout li")
+    pet_names.each do |pet|
+      name = pet.children[0].children[0].text
+
+    end
+  end
 
 end
