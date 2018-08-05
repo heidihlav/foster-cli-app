8ac56b0bb6cf7439eb77e412555885aa6c5212b9
Heidi Hlavinka <heidi@Heidis-MacBook-Pro.local>
2018-08-05 16:32:29 -0500

    Tried to find html tags which would avoid scraping registration form at bottom of li tags.

--- lib/foster/scraper.rb
+++ lib/foster/scraper.rb
@@ -2,16 +2,13 @@ class Foster::Scraper
 
   def self.scrape_pets
     doc = Nokogiri::HTML(open('http://www.friends4life.org/how-to-help/foster'))
-    doc.css(".callout li").each do |pet|
-      case
-      when pet.name != 0
-        puts name = pet.children[0].children[0].text
-        puts url = pet.children[0].attributes["href"].value
+    doc.css(".callout ul li:not(:last-child)").each do |pet|
+        name = pet.children[0].children[0].text
+        binding.pry
+
+        url = pet.children[0].attributes["href"].value
         Foster::Pets.new(name, url)
-      else pet.name == 0
-        puts "Not a pet."
       end
-    end
     # a_pet
   end
 
