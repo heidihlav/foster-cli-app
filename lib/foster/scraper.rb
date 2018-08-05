70c449fb7ae89296bae36323aa10cfa7f07f8b0b
Heidi Hlavinka <heidi@Heidis-MacBook-Pro.local>
2018-08-05 16:40:07 -0500

    Last commit before scraping different section of same website.

--- lib/foster/scraper.rb
+++ lib/foster/scraper.rb
@@ -2,10 +2,8 @@ class Foster::Scraper
 
   def self.scrape_pets
     doc = Nokogiri::HTML(open('http://www.friends4life.org/how-to-help/foster'))
-    doc.css(".callout ul li:not(:last-child)").each do |pet|
+    doc.css(".callout li").each do |pet|
         name = pet.children[0].children[0].text
-        binding.pry
-
         url = pet.children[0].attributes["href"].value
         Foster::Pets.new(name, url)
       end
