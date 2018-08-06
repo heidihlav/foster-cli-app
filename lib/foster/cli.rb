7b363ada97c73424a424a1deea2cb9398ab15a2a
Heidi Hlavinka <heidi@Heidis-MacBook-Pro.local>
2018-08-05 19:58:30 -0500

    Found @@all returned pet objects as array inside of array.

--- lib/foster/cli.rb
+++ lib/foster/cli.rb
@@ -10,23 +10,29 @@
     def list_pets
       puts "These are our cats or dogs available for foster:"
       @pets = Foster::Pets.all #returns objects
-      @pets.map.with_index(1) do |pet, index|
+      @pets_ary = @pets[21]
+      @pets_ary.map.with_index(1) do |pet, index|
         puts "#{index}. #{pet.name}"
-        # binding.pry
       end
     end
 
     def menu
-      user_input = nil
+      user_input = gets.strip
       while user_input != "exit"
         puts "Which cat or dog would you like to know more about? Type the number next to the pet's name. Type 'exit' if you are done. Or type 'list' to see the available pets again."
         user_input = gets.strip
 
-        if user_input.to_i > 0
-          the_pet = @pets[user_input.to_i - 1]
-          puts "#{the_pet.url}"
+        if user_input.to_i - 1 < 23
+          @pets_ary = @pets[21]
+          the_pet = @pets_ary[user_input.to_i]
+          # binding.pry
+          puts ""
+          puts "Visit 'www.friends4life.org#{the_pet.url}' to find out more about #{the_pet.name}!"
+          puts ""
         elsif user_input == "list"
           list_pets
+        elsif user_input == "exit"
+          goodbye
         else
           puts "Enter 'list' or 'exit'."
         end
--- lib/foster/scraper.rb
+++ lib/foster/scraper.rb
@@ -4,11 +4,10 @@ class Foster::Scraper
     doc = Nokogiri::HTML(open('http://www.friends4life.org/how-to-help/foster'))
     doc.css(".callout ul li").map do |pet|
         name = pet.children[0].children[0].text
-        # binding.pry
         url = pet.children[0].attributes["href"].value
         Foster::Pets.new(name, url)
     end
-    # a_pet
+    # testing
   end
 
 
