9ae331e563cd3e6fb3c34b6293f5b5be5b57138e
Heidi Hlavinka <heidi@Heidis-MacBook-Pro.local>
2018-08-08 20:48:35 -0500

    Edited scrape method - changed to .each and called scraper variables themselves, not setting them equal to another variable.

--- lib/foster/scraper.rb
+++ lib/foster/scraper.rb
@@ -2,11 +2,11 @@ class Foster::Scraper
 
   def self.scrape_pets
     doc = Nokogiri::HTML(open('http://www.friends4life.org/how-to-help/foster'))
-    doc.css(".callout ul li").map do |pet|
-        pet_names = pet.children[0].children[0].text
-        pet_urls = pet.children[0].attributes["href"].value
-        url = pet_urls
-        name = pet_names
+    doc.css(".callout ul li").each do |pet|
+        name = pet.children[0].children[0].text
+        url = pet.children[0].attributes["href"].value
+        name
+        url
         Foster::Pets.new(name, url)
     end
   end
