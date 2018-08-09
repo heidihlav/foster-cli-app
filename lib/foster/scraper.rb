45c7c810723367d687f707c711caf97916bca954
Heidi Hlavinka <heidi@Heidis-MacBook-Pro.local>
2018-08-08 20:50:28 -0500

    Deleted variable calls in scraper method.

--- lib/foster/scraper.rb
+++ lib/foster/scraper.rb
@@ -5,8 +5,6 @@ class Foster::Scraper
     doc.css(".callout ul li").each do |pet|
         name = pet.children[0].children[0].text
         url = pet.children[0].attributes["href"].value
-        name
-        url
         Foster::Pets.new(name, url)
     end
   end
